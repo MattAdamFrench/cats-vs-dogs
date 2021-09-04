140466071128ad140ad264d13d771194005007ae
MattFrench019 <matthewfrench019@gmail.com>
2021-09-04 15:24:47 +0100

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,57 @@
 import React from "react";
+import {Button, Container, Grid, makeStyles, Paper, Typography} from "@material-ui/core";
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000"
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+const useStyles = makeStyles((theme) => ({
+    pageContainer: {
+        padding: theme.spacing(3)
+    },
+    grid: {
+        paddingBottom: theme.spacing(1)
+    },
+    imgContainer: {
+        paddingLeft: theme.spacing(3),
+        paddingRight: theme.spacing(3),
+    },
+    img: {
+        width: "100%",
+        height: "100%",
+        maxWidth: "400px",
+        maxHeight: "300px",
+    },
+    input: {
+        display: "none"
+    }
+}));
+
+const roundTo2DP = number => {
+    return Math.round(number * 100) / 100
+}
 
 function App() {
+    const classes = useStyles();
+
     const [selectedFile, setFile] = React.useState(null);
+    const [objURL, setObjURL] = React.useState(null);
     const [cat, setCat] = React.useState("")
     const [dog, setDog] = React.useState("")
 
     const onChangeHandler = event => {
-        setFile(event.target.files[0]);
+        try {
+            if (event.target.files.length > 0) {
+                setFile(event.target.files[0]);
+                setObjURL(URL.createObjectURL(event.target.files[0]))
+            }
+        }
+        catch (e) {
+            console.warn("Handled Error: " + e)
+        }
     }
 
     const onClickHandler = event => {
         const data = new FormData();
         data.append("image", selectedFile)
-
-        console.log(process.env)
 
         fetch(apiUrl + "/submit", {
             method: "post",
@@ -26,20 +62,53 @@ function App() {
                 return res.json()
             })
             .then(json => {
-                setCat("Cat: " + (json.cat * 100) + "%")
-                setDog("Dog: " + (json.dog * 100) + "%")
+                setCat("Cat: " + roundTo2DP(json.cat * 100) + "%")
+                setDog("Dog: " + roundTo2DP(json.dog * 100) + "%")
             })
     }
 
     return (
-        <div>
-            <input type="file" name="file" onChange={onChangeHandler}/>
-            <button onClick={onClickHandler}>
-                Submit
-            </button>
-            <div>{cat}</div>
-            <div>{dog}</div>
-        </div>
+        <Container maxWidth="xs" className={classes.pageContainer}>
+            <Paper>
+                <Grid container direction="column" alignItems="center" spacing={2} className={classes.grid}>
+                    <Grid item>
+                        <Typography variant="h4">Cats vs Dogs</Typography>
+                    </Grid>
+                    <Grid item>
+                        <div className={classes.imgContainer}>
+                            {objURL !== null ? <img src={objURL} className={classes.img}/> : <div/>}
+                        </div>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body1">{cat}</Typography>
+                        <Typography variant="body1">{dog}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Grid container direction="row" spacing={2}>
+                            <Grid item>
+                                <input
+                                    accept="image/jpeg"
+                                    className={classes.input}
+                                    id="contained-button-file"
+                                    type="file"
+                                    onChange={onChangeHandler}
+                                />
+                                <label htmlFor="contained-button-file">
+                                    <Button variant="contained" color="primary" component="span">
+                                        Upload
+                                    </Button>
+                                </label>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="contained" color="primary" onClick={onClickHandler}>
+                                    Submit
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Container>
     );
 }
 
